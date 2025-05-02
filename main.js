3957fd3b7864173bab3cddca231a833a2e1bfafc
Victor Hugo Trujillo Charalla <htrujillocharalla@gmail.com>
2025-05-01 23:32:59 -0500

    encontrando errores

--- main.js
+++ main.js
@@ -7,35 +7,47 @@ const checkFile = (file) => {
 }
 
 const startCleanData = (data) => {
-    return data.replace("\r","").split(/\n/)
+    return data.replace(/\r/g, "").split(/\n/)
 }
 
 const validateDate = (dataDate) => /^\d{4}-\d{2}-\d{2}$/.test(dataDate)
 const stringNoNull = (text) => text.length !== 0
-const validateNumber = (num) => {return !isNaN(num) && Number(num) > 0 && Number.isInteger(num)} 
+const validateNumber = (num) => {return !isNaN(num) && Number(num) > 0 && Number.isInteger(Number(num))} 
 
 
 const readFile = (archivo) => {
     fs.readFile(archivo, 'utf8', (err, data) => {
         if (err) throw err;
-        cleanData = startCleanData(data);
+        const cleanData = startCleanData(data);
+        
 
         cleanData.forEach((line, idx) => {
-            const [dataDate, dataText, dataNum] = line.split(",");
+            if (line.trim() === "") return; 
+            
+            const lineNumber = idx + 1;
+            const parts = line.split(",");
+            const [dataDate, dataText, dataNum] = parts;
+            
+            
+            if (parts.length !== 3) {
+                console.log(`line ${lineNumber}: has an invalid format, must have 3 fields`);
+                return;
+            }
+            
 
-            console.log(dataDate,/^\d{4}-\d{2}-\d{2}$/.test(dataDate));
-            if(!validateDate(dataDate)) console.log(`line ${idx+1}: has an invalid date`);
-            if(!stringNoNull(dataText.trim())) console.log(`line ${idx+1}: has an invalid text`);
-            if(!validateNumber(dataNum)) console.log(`line ${idx+1}: has an invalid id`);
+
+            //console.log(dataDate,/^\d{4}-\d{2}-\d{2}$/.test(dataDate));
+            if(!validateDate(dataDate)) console.log(`line ${lineNumber}: Fecha Invalida`);
+            if(!stringNoNull(dataText?.trim())) console.log(`line ${lineNumber}: Texto invalido`);
+            if(!validateNumber(dataNum)) console.log(`line ${lineNumber}: ID Invalido`);
             
             
             
         });
+    });
+        
+      }
 
-        
-        
-      });
-}
 
 function main(){
     const archivo = process.argv[2]
